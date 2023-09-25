83e39d7f88aacf26ca93be87a0509bfec8b89d96
Jaejun Kim <jj.kim2@okestro.com>
2023-09-25 16:44:07 +0900

    any

--- section-2/basics-05-object-types/app.ts
+++ section-2/basics-05-object-types/app.ts
@@ -2,19 +2,14 @@
 //   name: string;
 //   age: number;
 // } = {
-const person :{
-  name:string;
-  age:number;
-  hobbies:string[];
-  role:[number,string];
-}={
+enum Role{ADMIN,READ_ONLY,AUTHOR};
+
+const person = {
   name: 'maximilian',
   age : 30,
-  hobbies:['sports','Cooking'],
-  role:[2,'author']
+  role:Role.ADMIN
 };
-let favoriteActivities: string[];
-favoriteActivities = ['sports'];
-
-
-console.log(person.name);
+let test_array:any; // 이딴 식을 쓰지마삼 any는 꼭 필요할때만 사용하기
+test_array = ['sports',3]
+console.log(person.role);
+console.log(Role[person.role]);
