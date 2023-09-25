7eca105b089916aa168f7ddcc13ca4c8ed7de9dd
Jaejun Kim <jj.kim2@okestro.com>
2023-09-25 15:44:58 +0900

    16. 객체형태

---
+++ section-2/basics-05-object-types/app.ts
@@ -0,0 +1,13 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person :{
+  name: String,
+  age : number
+}= {
+  name: 'Maximilian',
+  age: 30
+};
+
+console.log(person.name);
