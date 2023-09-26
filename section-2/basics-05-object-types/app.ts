a7314ffdb8e345c84db91e806e783e50bc7cd628
Jaejun Kim <jj.kim2@okestro.com>
2023-09-26 14:44:16 +0900

    30. never 타입

--- section-2/basics-05-object-types/app.ts
+++ section-2/basics-05-object-types/app.ts
@@ -1,6 +1,14 @@
-let userInput:any;
+let userInput:unknown;
 let userName:string;
 
 userInput = 'Max';
 
-userName = userInput;
+if(typeof userInput ==='string'){
+    userName = userInput;
+}
+function generateError(message:string,code:number):never{
+    throw {message:message, errorCode : code};
+}
+const result = generateError('An error occured',500);
+
+console.log(result);
