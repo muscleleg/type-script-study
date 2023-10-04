d20e1a31b815f235a0e3a93586f474b2ef6ea4f5
Jaejun Kim <jj.kim2@okestro.com>
2023-10-04 11:13:33 +0900

    53.나머지 매개변수
    일부러 에러냈음
    reduce에 대한 내용, 가변 파라미터

--- section-4/next-gen-01-starting-setup/src/app.ts
+++ section-4/next-gen-01-starting-setup/src/app.ts
@@ -1,10 +1,26 @@
-// Code goes here!
+// const add = (a, b) => a + b;
+// const printOutput = output => console.log(output);
+// const button = document.querySelector('button');
+// if (button) {
+//     button.addEventListener('click', event => console.log(event));
+// }
+// printOutput(add(5, 2));
 
-const add = (a:number, b:number) => a+b;
+const hobbies = ['Sports','Cooking'];
+const activeHobbies = ['Hiking'];
 
-const printOutput:(a:number | string) => void = output => console.log(output);
-const button = document.querySelector('button');
-if(button){
-    button.addEventListener('click',event=>console.log(event));
-}
-printOutput(add(5,2));
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+
+const person = {
+    name:'Max',
+    age:30
+};
+
+const copiedPerson = {...person};
+
+console.log(copiedPerson);
+
+const add = (...numbers:[number,number,number])=> numbers.reduce((curResult,curValue)=>curResult+curValue,0);
+const addedNumbers = add(5,10,2,3.7);
+console.log(addedNumbers);
