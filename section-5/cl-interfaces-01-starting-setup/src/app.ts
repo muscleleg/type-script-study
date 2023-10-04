a77e26519149d6ed530c6fad91966e496e1113a3
Jaejun Kim <jj.kim2@okestro.com>
2023-10-04 15:20:46 +0900

    62. 개인 및 "공용" 액세스 수정자
    외부객체에 클래스 메소드를 참조시키고 사용해보는 연습해봄
    외부객체에 클래스 메소드를 참조시켜 사용하고 싶다면 해당 클래스가 public이여야만함
    왜냐하면 this를 통해서 사용하게 되는데 외부객체임으로 private일 경우 막혀서 사용이 안됨

--- section-5/cl-interfaces-01-starting-setup/src/app.ts
+++ section-5/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,6 +1,8 @@
 // Code goes here!
 class Department{
-    name:string;
+    public name:string;
+    // public employees:string[] = [];
+    private employees:string[] = [];
 
     constructor(n: string) {
         this.name = n;
@@ -8,10 +10,31 @@ class Department{
     describe(this:Department){
         console.log('Department: '+ this.name);
     }
+    addEmployee(employee:string){
+        this.employees.push(employee);
+    }
+    printEmployeeInformation(){
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department('Accounting');
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.printEmployeeInformation();
+
+
 accounting.describe();
 
-const accountingCopy = {name:'test',describe: accounting.describe};
-accountingCopy.describe();
+// const accountingCopy :Department = {
+//     name:'test',
+//     employees:[],
+//     describe: accounting.describe,
+//     addEmployee:accounting.addEmployee,
+//     printEmployeeInformation:accounting.printEmployeeInformation
+// };
+// accountingCopy.describe();
+// accountingCopy.addEmployee("메소드 삽입테스트")
+// accountingCopy.printEmployeeInformation();
