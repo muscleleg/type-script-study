3d58ebe1ddb9182180a879f94ff8348b03de9553
Jaejun Kim <jj.kim2@okestro.com>
2023-10-04 10:34:28 +0900

    init section 4

---
+++ section-4/next-gen-01-starting-setup/src/app.ts
@@ -0,0 +1,10 @@
+// Code goes here!
+
+const add = (a:number, b:number) => a+b;
+
+const printOutput:(a:number | string) => void = output => console.log(output);
+const button = document.querySelector('button');
+if(button){
+    button.addEventListener('click',event=>console.log(event));
+}
+printOutput(add(5,2));
