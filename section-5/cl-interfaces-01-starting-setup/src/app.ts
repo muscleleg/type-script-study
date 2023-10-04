b38050e5e8e58fa27ad2de7f67686ae9d0194bf8
Jaejun Kim <jj.kim2@okestro.com>
2023-10-04 17:10:28 +0900

    67. 게터 & 세터

--- section-5/cl-interfaces-01-starting-setup/src/app.ts
+++ section-5/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,56 +1,84 @@
 // Code goes here!
-class Department{
+class Department {
     // private id:string;
     // private name:string;
     // public employees:string[] = [];
-    protected employees:string[] = [];
+    protected employees: string[] = [];
 
-    constructor(private readonly id:string, public name: string) {
+    constructor(private readonly id: string, public name: string) {
         // this.id = id;
         // this.name = n;
     }
-    describe(this:Department){
+
+    describe(this: Department) {
         console.log(`Department(${this.id}): ${this.name}`);
     }
-    addEmployee(employee:string){
+
+    addEmployee(employee: string) {
         this.employees.push(employee);
     }
-    printEmployeeInformation(){
+
+    printEmployeeInformation() {
         console.log(this.employees.length);
         console.log(this.employees);
     }
 }
-class ITDepartment extends Department{
-    admins:string[];
-    constructor(id:string, admins:string[]) {
-        super(id,'IT');
+
+class ITDepartment extends Department {
+    admins: string[];
+
+    constructor(id: string, admins: string[]) {
+        super(id, 'IT');
         this.admins = admins;
     }
+
     // describe() {
     //     super.describe();
     //     console.log(`admins: ${this.admins}`)
     // }
 
 }
-class AccountingDepartment extends Department{
-    constructor(id:string, private reports:string[]) {
-        super(id,'IT');
+
+class AccountingDepartment extends Department {
+    private lastReports: string;
+
+    get mostRecentReport() {
+        if (this.lastReports) {
+            return this.lastReports;
+        }
+        throw new Error('No report found.');
     }
+
+    set mostRecentReport(value:string){
+        if(!value){
+            throw new Error('Please pass in a valid value!');
+        }
+        this.addReport(value)
+    }
+
+    constructor(id: string, private reports: string[]) {
+        super(id, 'IT');
+        this.lastReports = reports[0];
+    }
+
     addEmployee(name: string) {
-        if(name === 'Max'){
+        if (name === 'Max') {
             return;
         }
         this.employees.push(name);
     }
 
-    addReport(text:string){
+    addReport(text: string) {
         this.reports.push(text);
+        this.lastReports = text;
     }
-    printReports(){
+
+    printReports() {
         console.log(this.reports);
     }
 
 }
+
 // const accounting = new Department('d1','Accounting');
 // const itDepartment = new ITDepartment('d1',['Accounting']);
 //
@@ -59,8 +87,13 @@ class AccountingDepartment extends Department{
 // itDepartment.printEmployeeInformation();
 // itDepartment.describe();
 
-const accounting = new AccountingDepartment('d2',[]);
+const accounting = new AccountingDepartment('d2', []);
+
+accounting.mostRecentReport = '';
 accounting.addReport('something went wrong');
+
+console.log(accounting.mostRecentReport);
+
 accounting.addEmployee('Max');
 accounting.addEmployee('Manu');
 
